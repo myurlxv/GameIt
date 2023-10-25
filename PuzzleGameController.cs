85e3b18fbf64dee5f64ecc7db9b1427d7366fc62
Hyun <128339058+myurlxv@users.noreply.github.com>
2023-10-25 21:07:56 +0900

    Add files via upload
    쓰리매칭퍼즐게임... 을 만들고 싶었습니다...
    사실 이거 말고 다른 코드가 있는데 오류나는 바람에 대안으로 이 코드라도 올립니다...
    기말고사 때는 좀 더 열심히 해보겠습니다... 감사합니다.

---
+++ PuzzleGameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleGameController : MonoBehaviour
+{
+    public GameObject blockPrefab;
+    public int boardWidth = 5;
+    public int boardHeight = 5;
+
+    private GameObject[,] board;
+
+    void Start()
+    {
+        InitializeBoard();
+    }
+
+    void InitializeBoard()
+    {
+        board = new GameObject[boardWidth, boardHeight];
+
+        for (int x = 0; x < boardWidth; x++)
+        {
+            for (int y = 0; y < boardHeight; y++)
+            {
+                Vector3 position = new Vector3(x, y, 0);
+                GameObject block = Instantiate(blockPrefab, position, Quaternion.identity);
+                board[x, y] = block;
+            }
+        }
+    }
+
+    // Add code for block swapping, matching, scoring, and game over conditions here.
+}
